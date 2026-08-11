1561c572c49921a2286b4211b44e1b58cb17039c
Aman Pandey <01aman2005@gmail.com>
2026-08-11 22:00:53 +0530

    Time: 4 ms (96.48%), Space: 49.6 MB (49.32%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -1,26 +1,26 @@
-class Solution {
-    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int count=0;
-        if(ruleKey.equals("type")){
-            count = search(items,0,ruleValue);
-        }else if(ruleKey.equals("color")){
-            count=search(items,1,ruleValue);
-        }else{
-            count=search(items,2,ruleValue);
-        }
-        return count;
-    }
+// class Solution {
+//     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+//         int count=0;
+//         if(ruleKey.equals("type")){
+//             count = search(items,0,ruleValue);
+//         }else if(ruleKey.equals("color")){
+//             count=search(items,1,ruleValue);
+//         }else{
+//             count=search(items,2,ruleValue);
+//         }
+//         return count;
+//     }
 
-    int search(List<List<String>> items,int column,String target){
-        int count =0;
-        for(int i=0;i<items.size();i++){
-            if(items.get(i).get(column).equals(target)){
-                count++;
-            }      
-        }
-        return count;
-    }
-}
+//     int search(List<List<String>> items,int column,String target){
+//         int count =0;
+//         for(int i=0;i<items.size();i++){
+//             if(items.get(i).get(column).equals(target)){
+//                 count++;
+//             }      
+//         }
+//         return count;
+//     }
+// }
 
 // class Solution {
 //     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
@@ -37,3 +37,21 @@ class Solution {
 //     }
 // }
 
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count = 0;
+
+        int ruleKeyValue = -1;
+        if (ruleKey.equals("type")) ruleKeyValue = 0;
+        else if (ruleKey.equals("color")) ruleKeyValue = 1;
+        else ruleKeyValue = 2;
+
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).get(ruleKeyValue).equals(ruleValue)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

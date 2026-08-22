c194870760917af20b78fccd6630b58ef55fed9f
Aman Pandey <01aman2005@gmail.com>
2026-08-22 15:57:35 +0530

    Time: 1 ms (45.25%), Space: 43.2 MB (84.84%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximumPopulation(int[][] logs) {
+
+        int[] population = new int[101];
+
+        for (int i = 0; i < logs.length; i++) {
+            int birth = logs[i][0];
+            int death = logs[i][1];
+
+            for (int year = birth; year < death; year++) {
+                population[year - 1950]++;
+            }
+        }
+
+        int maxPopulation = 0;
+        int currentYear = 1950;
+
+        for (int i = 0; i < population.length; i++) {
+
+            if (population[i] > maxPopulation) {
+                maxPopulation = population[i];
+                currentYear = i + 1950;
+            }
+        }
+
+        return currentYear;
+    }
+}

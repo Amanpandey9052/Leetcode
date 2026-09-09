b882b76b9a0b4e826db0038c24e1564bb75ea390
Aman Pandey <01aman2005@gmail.com>
2026-09-09 21:03:53 +0530

    Time: 1 ms (3.06%), Space: 43.4 MB (61.24%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,16 +2,13 @@ class Solution {
     public int[] plusOne(int[] digits) {
         int n=digits.length-1;
         List<Integer> list = new ArrayList<>();
-        int k=1;
-        int c=0; 
+        int carry=1; 
 
-        while(n>=0 || k>0 || c>0){
-            int digit=k%10;
+        while(n>=0 ||carry>0){
             int newSum=(n>=0)?digits[n]:0;
-            int sum=newSum+k+c;
+            int sum=newSum+carry;
             list.add(sum%10);
-            c=sum/10;
-            k=k/10;
+            carry=sum/10;
             n--;
         }
 
